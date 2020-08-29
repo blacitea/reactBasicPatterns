c43f5e91dfa588dfcdd5d18fae57174d79dfa02f
Angel Tsang <65837801+blacitea@users.noreply.github.com>
2020-08-29 15:27:52 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.scss";
+
+function Application(props) {
+  return <main />;
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"));
