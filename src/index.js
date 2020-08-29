8ccaf9287e4aaa24b91e15c13bd10b3539a048ea
Angel Tsang <65837801+blacitea@users.noreply.github.com>
2020-08-29 15:41:40 -0700

    create button component access data from children by using {props.children}

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./styles.scss";
 
+function Button(props) {
+  return <button>{props.children}</button>;
+}
+
 function Application(props) {
-  return <main />;
+  return (
+    <main>
+      <Button>Reset</Button>
+    </main>
+  );
 }
 
 ReactDOM.render(<Application />, document.getElementById("root"));
